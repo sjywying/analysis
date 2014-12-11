831ae929d600692ef9bb8af9fcdaee3dd1687d21
sunjianye <sunjianye@wukong.com>
2014-12-11 10:31:31 +0800

    init

--- analysis-web/src/main/java/com/analysis/web/mapper/provider/RegisterProvider.java
+++ analysis-web/src/main/java/com/analysis/web/mapper/provider/RegisterProvider.java
@@ -9,23 +9,25 @@ public class RegisterProvider {
 	
 	private static final String TABLE_NAME_PREFIX = "biz_register_";
     
-	public String insert(Register registe) {
-//		Registe registe = (Registe) parameters.get("registe");
-		String ctime = registe.getCtime().substring(0, 7).replace("-", "");
+	public String insert(Register register) {
+		//TODO 源头格式化
+		String ctime = register.getCtime().substring(0, 7).replace("-", "");
 		
 		SQL sql = new SQL();
 		sql.INSERT_INTO(TABLE_NAME_PREFIX + ctime)
-			.VALUES("tid", Constants.SQL_SINGLE_QUOTES + registe.getTid() + Constants.SQL_SINGLE_QUOTES)
-			.VALUES("ctime", Constants.SQL_SINGLE_QUOTES + registe.getCtime() + Constants.SQL_SINGLE_QUOTES)
-			.VALUES("imsi", Constants.SQL_SINGLE_QUOTES + registe.getImsi() + Constants.SQL_SINGLE_QUOTES)
-			.VALUES("imei", Constants.SQL_SINGLE_QUOTES + registe.getImei() + Constants.SQL_SINGLE_QUOTES)
-			.VALUES("channel", Constants.SQL_SINGLE_QUOTES + registe.getC() + Constants.SQL_SINGLE_QUOTES)
-			.VALUES("model", Constants.SQL_SINGLE_QUOTES + registe.getM() + Constants.SQL_SINGLE_QUOTES);
+			.VALUES("tid", Constants.SQL_SINGLE_QUOTES + register.getTid() + Constants.SQL_SINGLE_QUOTES)
+			.VALUES("ctime", Constants.SQL_SINGLE_QUOTES + register.getCtime() + Constants.SQL_SINGLE_QUOTES)
+			.VALUES("imsi", Constants.SQL_SINGLE_QUOTES + register.getImsi() + Constants.SQL_SINGLE_QUOTES)
+			.VALUES("imei", Constants.SQL_SINGLE_QUOTES + register.getImei() + Constants.SQL_SINGLE_QUOTES)
+			.VALUES("channel", Constants.SQL_SINGLE_QUOTES + register.getC() + Constants.SQL_SINGLE_QUOTES)
+			.VALUES("model", Constants.SQL_SINGLE_QUOTES + register.getM() + Constants.SQL_SINGLE_QUOTES)
+			.VALUES("ccode", Constants.SQL_SINGLE_QUOTES + register.getCcode() + Constants.SQL_SINGLE_QUOTES);
 		
-		if(registe.getAv() != null) sql.VALUES("av", Constants.SQL_SINGLE_QUOTES + registe.getAv() + Constants.SQL_SINGLE_QUOTES);
-		if(registe.getR() != null) sql.VALUES("an", Constants.SQL_SINGLE_QUOTES + registe.getR() + Constants.SQL_SINGLE_QUOTES);
-		if(registe.getIp() != null) sql.VALUES("ip", Constants.SQL_SINGLE_QUOTES + registe.getIp() + Constants.SQL_SINGLE_QUOTES);
-		if(registe.getType() != null) sql.VALUES("type", Constants.SQL_SINGLE_QUOTES + registe.getType() + Constants.SQL_SINGLE_QUOTES);
+		if(register.getAv() != null) sql.VALUES("av", Constants.SQL_SINGLE_QUOTES + register.getAv() + Constants.SQL_SINGLE_QUOTES);
+		if(register.getR() != null) sql.VALUES("an", Constants.SQL_SINGLE_QUOTES + register.getR() + Constants.SQL_SINGLE_QUOTES);
+		if(register.getIp() != null) sql.VALUES("ip", Constants.SQL_SINGLE_QUOTES + register.getIp() + Constants.SQL_SINGLE_QUOTES);
+		if(register.getType() != null) sql.VALUES("type", Constants.SQL_SINGLE_QUOTES + register.getType() + Constants.SQL_SINGLE_QUOTES);
+		if(register.getPname() != null) sql.VALUES("pname", Constants.SQL_SINGLE_QUOTES + register.getPname() + Constants.SQL_SINGLE_QUOTES);
 		
 		return sql.toString();
 	}
