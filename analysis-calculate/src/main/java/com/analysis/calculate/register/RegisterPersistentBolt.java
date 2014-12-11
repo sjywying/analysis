3b2022923626cdb445140bf2da9c8643750f3ff0
sunjianye <sunjianye@wukong.com>
2014-12-11 11:09:51 +0800

    init

--- analysis-calculate/src/main/java/com/analysis/calculate/register/RegisterPersistentBolt.java
+++ analysis-calculate/src/main/java/com/analysis/calculate/register/RegisterPersistentBolt.java
@@ -45,6 +45,8 @@ public class RegisterPersistentBolt implements IRichBolt {
 //				TODO transaction
 				redisTemplate.opsForSet().add(RedisConstants.REGISTE_SET_TID_MEMCACHE, tid);
 				redisTemplate.opsForHash().putIfAbsent(RedisConstants.REGISTE_HASH_CONTENT_ACTIVE, tid, content);
+			} else {
+				redisTemplate.opsForHash().put(RedisConstants.REGISTE_HASH_CONTENT_ERROR, tid, content+"#isexisttrue#");
 			}
 			
 		} catch (Exception e) {
