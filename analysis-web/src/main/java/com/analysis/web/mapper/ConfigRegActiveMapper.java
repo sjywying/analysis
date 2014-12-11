eb6ca4e05df8b16e09bfeeb680831e9453602392
sunjianye <sunjianye@wukong.com>
2014-12-11 19:50:26 +0800

    init

--- analysis-web/src/main/java/com/analysis/web/mapper/ConfigRegActiveMapper.java
+++ analysis-web/src/main/java/com/analysis/web/mapper/ConfigRegActiveMapper.java
@@ -2,13 +2,13 @@ package com.analysis.web.mapper;
 
 import org.apache.ibatis.annotations.InsertProvider;
 
-import com.analysis.api.bean.Register;
+import com.analysis.api.bean.ConfigRegActive;
 import com.analysis.web.mapper.provider.ConfigRegActiveProvider;
 
 
 public interface ConfigRegActiveMapper {
 	
 	@InsertProvider(type=ConfigRegActiveProvider.class, method="insert")
-	void insert(Register registe);
+	void insert(ConfigRegActive bean);
     
 }
