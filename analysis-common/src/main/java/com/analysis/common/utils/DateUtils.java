4d6d50b6888be3d0aea11634d39dab80f899c424
sunjianye <sunjianye@wukong.com>
2014-12-11 10:58:00 +0800

    init

---
+++ analysis-common/src/main/java/com/analysis/common/utils/DateUtils.java
@@ -0,0 +1,24 @@
+package com.analysis.common.utils;
+
+import java.text.SimpleDateFormat;
+
+public class DateUtils {
+
+//	private static final SimpleDateFormat commondf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//	private static final SimpleDateFormat yyyyMMddHHmmss = new SimpleDateFormat("yyyyMMddHHmmss");
+	
+	/**
+	 * "yyyy-MM-dd HH:mm:ss"转换为"yyyyMMddHHmmss"
+	 * 
+	 * @param dateStr
+	 * @return
+	 * @throws RuntimeException
+	 */
+	public static String transition(String dateStr) throws RuntimeException {
+		if(Strings.isEmpty(dateStr)) {
+			throw new RuntimeException("date string is empty");
+		}
+		
+		return dateStr.replace("-", "").trim();
+	}
+}
