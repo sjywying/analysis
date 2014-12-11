8b234c024830f4cbb7774d450554f1e57c7af898
sunjianye <sunjianye@wukong.com>
2014-12-11 16:00:36 +0800

    init

--- analysis-common/src/main/java/com/analysis/common/constants/RedisConstants.java
+++ analysis-common/src/main/java/com/analysis/common/constants/RedisConstants.java
@@ -2,15 +2,16 @@ package com.analysis.common.constants;
 
 public interface RedisConstants {
 	
-	/*******************仅仅在redis中存储注册的tid集合*********************/
 	public static final String REGISTE_SET_TID_MEMCACHE = "reg_tid_memcache";
-	/*******************在redis,mysql中存储注册的tid集合*********************/
 	public static final String REGISTE_SET_TID_PERSISTENT = "reg_tid_persistent";
-	/*******************以此值为key，tid为filed进行hash存储*********************/
 	public static final String REGISTE_HASH_CONTENT_ACTIVE = "reg_content";
-	/**************以此值为key，tid为filed进行hash存储异常数据*********************/
 	public static final String REGISTE_HASH_CONTENT_ERROR = "reg_content_error";
 	
+	
+	public static final String CONFIGACTIVE_SET_TID_MEMCACHE = "configactive_tid_memcache";
+	public static final String CONFIGACTIVE_SET_TID_PERSISTENT = "configactive_tid_persistent";
+	public static final String CONFIGACTIVE_HASH_CONTENT = "configactive_content";
+	public static final String CONFIGACTIVE_HASH_CONTENT_ERROR = "configactive_content_error";
 	
 	
 	public static final String REGACTIVE_SET_TID_MEMCACHE = "regactive_tid_memcache";
